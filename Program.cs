d8d0ef85120a5fa0dd31b78bf8aba7bd7bb5fd7f
Hafsat Dere <derehafsat24@gmail.com>
2026-01-21 20:24:59 +0100

    class and object

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+class Person
+{
+    public string fullname = "Zaid Amina";
+    public int age = 16;
+    public void Introduce()
+    {
+        Console.WriteLine($"My name is {fullname} and i am {age} years old");
+    }
+
+}
+class Student : Person
+{
+    public int studentNumber = 2;
+    public void Study()
+    {
+        Console.WriteLine("student is studying");
+    }
+
+    void Main()
+    {
+        Student captain = new Student();
+        captain.Introduce();
+        captain.Study();
+    }
+}
+class Teacher : Person
+{
+    string subject;
+    void Teach()
+    {
+        Console.WriteLine($"Teacher is teaching {subject}");
+    }
+}
